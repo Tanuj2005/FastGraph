92c192b70524ff746e25e9b62293f8e6d3bbbf2f
Tanuj2005 <tanujtyagiofficial@gmail.com>
2026-05-17 12:06:33 +0530

    resp hpp

--- src/protocol/resp.hpp
+++ src/protocol/resp.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include <string>
+#include <vector>
+
+enum class ParseResult {
+    Complete,
+    Incomplete,
+    Error
+};
+
+class RespParser {
+public:
+    ParseResult parse( std::string& buf, std::vector<std::string>& out ) ;
+
+private:
+    ParseResult parse_array( std:: string& buf, std::vector<std::string>& out ) ;
+    ParseResult parse_inline( std::string& buf, std::vector<std::string>& out ) ;
+    int find_crlf( const std::string& buf, size_t start = 0 ) ;
+};
