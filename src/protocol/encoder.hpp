84e39a94b543dc3935f62a4b837b67c1140b77a3
Tanuj2005 <tanujtyagiofficial@gmail.com>
2026-05-17 13:01:50 +0530

    encoder hpp

--- src/protocol/encoder.hpp
+++ src/protocol/encoder.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include <string>
+#include <vector>
+
+class RespEncoder {
+public:
+    static std::string simple_string( const std::string& s ) ;
+    static std::string error( const std::string& msg ) ;
+    static std::string integer( long long n ) ;
+    static std::string bulk_string( const std::string& s ) ;
+    static std::string null_bulk() ;
+    static std::string array( const std::vector<std::string>& items ) ;
+    static std::string empty_array() ;
+};
