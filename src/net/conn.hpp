2671dde283f0d0f4663bd6e61bd8f977023eecbe
Tanuj2005 <tanujtyagiofficial@gmail.com>
2026-05-18 12:38:16 +0530

    conn hpp

--- src/net/conn.hpp
+++ src/net/conn.hpp
@@ -1,1 +1,15 @@
 #pragma once
+#include <string>
+#include "protocol/resp.hpp"
+
+enum class ConnState { Reading, Writing, Closing };
+
+struct Conn {
+    int        fd;
+    ConnState  state = ConnState::Reading;
+    std::string read_buf;
+    std::string write_buf;
+    RespParser  parser;
+
+    explicit Conn(int fd) : fd(fd) {}
+};
